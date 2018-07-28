5528ce96dc31e062b0db594fc757db2a6854c014
lal <eduardo.rasgado@hotmail.com>
2018-07-28 06:48:39 -0500

    logic for timeouts

--- js/keybon.js
+++ js/keybon.js
@@ -2,12 +2,12 @@
 let tempo = 0
 //startShow = [81,65,90,87,83,88,69,68,67,82,70,86,84,71,66,89,72,78,85,74,77,73,75,79,76,80]
 
-// const intervalo = setInterval(() => {
-// 	if (tempo <=25){
-// 		activate(65+tempo, { success: false, fail: false })
-// 		tempo++
-// 	}
-// }, 20)
+const intervalo = setInterval(() => {
+	if (tempo <=25){
+		activate(65+tempo, { success: false, fail: false })
+		tempo++
+	}
+}, 20)
 
 //-------------------------
 
@@ -26,17 +26,53 @@ function generateRandomKey() {
 }
 
 function nextLevel(nivelActual) {
+	//si se llega al ultimo nivel y pasa la ronda
 	if(nivelActual == (niveles)) {
 		setTimeout(() => alert('Ganaste'), 100)
 	}
+	//cada nivel
+	confirm(`Nivel actual: ${nivelActual+1}`)
 
-	alert(`Nivel actual: ${nivelActual+1}`)
-
+	//mostrar las keys que debe tocar el usuario
 	for(let i = 0; i <= nivelActual; i++){
 		setTimeout(() => activate(keys[i]),1000*(i+1))
 	}
+
+	//dejar al usuario que comience a tocar las teclas(mimic)
+	let i = 0
+	let teclaActual = keys[i]
+
+	window.addEventListener('keydown', onkeyDown)
+
+	function onkeyDown(event) {
+		//en caso de acertar la tecla debida
+		if (event.keyCode == teclaActual) {
+			//activar la tecla en verde
+			activate(teclaActual, { success:true, fail: false})
+			//incrementa la flag para analizar la siguiente techa
+			i++
+			//si ya termina de poner todas las teclas en el nivel debido
+			if (i > nivelActual) {
+				//quitar la escucha
+				window.removeEventListener('keydown', onkeyDown)
+				//llamar al nivel siguiente
+				setTimeout(() => nextLevel(i), 1500)
+			}
+			//cambiar la tecla actual aqui porque no hay un ciclo para iterar
+			teclaActual = keys[i]
+		} 
+		//el caso erroneo de apretar la tecla
+		else {
+			//activar tecla erronea y tecla que debia ser apretada
+			activate(event.keyCode, {success: false, fail: true });
+		    activate(teclaActual, {});
+		    //apagar escucha
+		    window.removeEventListener('keydown', onkeydown);
+		    //alerta de fallo
+			setTimeout(() => alert('Perdiste :( / Game Over :('), 1000)
+		}
+	}
 }
-nextLevel(14)
 
 function getkeyCode(key) {
 	//entre corchetes para indicar que es un atributo
@@ -57,14 +93,26 @@ function deactivate(element) {
 	element.className = 'key'
 }
 
-function listener() {
-	document.addEventListener('keydown', function(event) {
-		activate(event.keyCode, { success: true, fail: false })
-		//parar la animacion de presentacion
-		// if (tempo > 26){
-		// 	clearInterval(intervalo)
-		// }
-	 } )
+function main() {
+		
+	//parar la animacion de presentacion
+	setTimeout(() => {
+		if (tempo > 26){
+			clearInterval(intervalo)
+		}
+		//confirmacion, si no hay juego entonces no hace nada
+		const play = confirm("Vas a jugar?/ Are you ready?")
+		if (play){
+			//inicia el juego
+			setTimeout(() => nextLevel(0),1000)
+		}
+	}, 1000)
+	
+	///setTimeout(() => nextLevel(0), 5000)
 
 }
-listener()
+
+//iniciar secuencia de juego luego de presentacion
+setTimeout(() => {
+	main()
+}, 3000)
